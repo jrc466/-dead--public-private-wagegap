c5dabf56f4fdd854f24a256841c77ae5823ed400
jrc466 <jrc466@cornell.edu>
2018-12-03 17:05:13 -0500

    setting parameters to debug

--- programs/config.R
+++ programs/config.R
@@ -15,6 +15,6 @@ debug = 1
 suffix = ifelse(debug == 1, "_debug","")
 
 # One percent sample
-onepc = 1
+onepc = 0
 suffix = ifelse(debug == 1 & onepc == 1, "_onepc",suffix)
 noboot = ifelse(debug == 1 & onepc == 0, TRUE,TRUE)

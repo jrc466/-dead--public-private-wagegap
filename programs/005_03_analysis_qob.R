c00e081af65543f3917ea7f806a797a13c5ee9c6
jrc466 <jrc466@cornell.edu>
2018-12-09 20:08:55 -0500

    fixing output names and changing order of some estimations

--- programs/005_03_analysis_qob.R
+++ programs/005_03_analysis_qob.R
@@ -34,26 +34,12 @@ meta.time.file = paste(metadata.dir,"005_elapsed.csv",sep="/")
 #########
 # 3. Oaxaca-Blinder counterfactual quantile
 #########
+
+setwd(analysis.dir)
+
 #Changing coding for group1 and group2 
 data[,group1:=ifelse(group1==1,0,1)]
 data[,group2:=ifelse(group2==1,0,1)]
-
-# Declaring basic formula
-base.qob = function(x){
-  counterfactual(log(hwage1)~nonwhite1+sex1+med_skill+high_skill+tenure+I(tenure^2)+factor(region)+factor(yr),
-                 data=x,group=group1,sepcore=T,treatment=T,decomposition=T,ncore=detectCores()-1,noboot=noboot,nreg=100)
-}
-
-#First, using the whole sample
-t = system.time(qob00 <- base.qob(data))
-
-file.name=paste("002_qob",suffix,".RData",sep="")
-this.meta.time = rbind(meta.time,list(file.name,as.character(Sys.Date()),t[[1]],t[[2]],t[[3]]))
-fwrite(this.meta.time,meta.time.file,append=T)
-
-setwd(analysis.dir)
-save(qob00,file=file.name)
-rm(qob00)
 
 #########
 # 3.1 Oaxaca-Blinder counterfactual quantile - by gender and skill
@@ -66,25 +52,13 @@ qob.gs = function(x){
 for (i in 0:1){
   for (j in 1:3){
     t = system.time((q <- qob.gs(data[sex1==i&skill==j])))
-    file.name = paste("002_qob_genderskill_",i,j,suffix,".RData",sep="")
+    file.name = paste("002_qob_genderskill_",i,j,suffix,out.se,".RData",sep="")
     this.meta.time = rbind(meta.time,list(file.name,as.character(Sys.Date()),t[[1]],t[[2]],t[[3]]))
     fwrite(this.meta.time,meta.time.file,append=T)
     save(q,file = file.name)
   }
 }
 rm(q)
-
-# qob01 = qob.gs(data[sex1==1&low_skill==1])
-# qob02 = qob.gs(data[sex1==1&med_skill==1])
-# qob03 = qob.gs(data[sex1==1&high_skill==1])
-# qob04 = qob.gs(data[sex1==0&low_skill==1])
-# qob05 = qob.gs(data[sex1==0&med_skill==1])
-# qob06 = qob.gs(data[sex1==0&high_skill==1])
-# 
-# prov = list(qob01,qob02,qob03,qob04,qob05,qob06)
-# save(prov,file=)
-# rm(prov)
-# rm(list=ls(pattern="qob"))
 
 #########
 # 3.2 Oaxaca-Blinder counterfactual quantile - by race and skill
@@ -97,25 +71,13 @@ qob.rs = function(x){
 for (i in 0:1){
   for (j in 1:3){
     t = system.time((q <- qob.rs(data[nonwhite1==i&skill==j])))
-    file.name = paste("003_qob_raceskill_",i,j,suffix,".RData",sep="")
+    file.name = paste("003_qob_raceskill_",i,j,suffix,out.se,".RData",sep="")
     this.meta.time = rbind(meta.time,list(file.name,as.character(Sys.Date()),t[[1]],t[[2]],t[[3]]))
     fwrite(this.meta.time,meta.time.file,append=T)
     save(q,file = file.name)
   }
 }
 rm(q)
-
-# qob07 = qob.rs(data[nonwhite1==1&low_skill==1])
-# qob08 = qob.rs(data[nonwhite1==1&med_skill==1])
-# qob09 = qob.rs(data[nonwhite1==1&high_skill==1])
-# qob10 = qob.rs(data[nonwhite1==0&low_skill==1])
-# qob11 = qob.rs(data[nonwhite1==0&med_skill==1])
-# qob12 = qob.rs(data[nonwhite1==0&high_skill==1])
-# 
-# prov = list(qob07,qob08,qob09,qob10,qob11,qob12)
-# save(prov,file=paste("003_qob_raceskill",suffix,".RData",sep=""))
-# rm(prov)
-# rm(list=ls(pattern="qob"))
 
 #########
 # 4 Inputing individual FEs
@@ -130,27 +92,7 @@ data = merge(data,fe.pis,by="pis")
 # 5 Recomputing QOB *~without~* fixed effects
 #########
 
-# 5.1 QOB on whole sample
-base.qob = function(x){
-  counterfactual(log(hwage1)-effect~nonwhite1+sex1+med_skill+high_skill+tenure+I(tenure^2)+factor(region)+factor(yr),
-                 data=x,group=group1,sepcore=T,treatment=T,decomposition=T,ncore=detectCores()-1,noboot=noboot,nreg=100)
-}
-
-t = system.time(qob00_net <- base.qob(data))
-
-file.name=paste("002_net_qob",suffix,".RData",sep="")
-this.meta.time = rbind(meta.time,list(file.name,as.character(Sys.Date()),t[[1]],t[[2]],t[[3]]))
-fwrite(this.meta.time,meta.time.file,append=T)
-
-setwd(analysis.dir)
-save(qob00_net,file=file.name)
-rm(qob00_net)
-
-# qob00_net = base.qob(data)
-# save(qob00_net,file=paste("002_net_qob",suffix,".RData",sep=""))
-# rm(qob00_net)
-
-# 5.2 QOB by gender and skill
+# 5.1 QOB by gender and skill
 qob.gs = function(x){
   counterfactual(log(hwage1)-effect~nonwhite1+tenure+I(tenure^2)+factor(region)+factor(yr),
                  data=x,group=group1,sepcore=T,treatment=T,decomposition=T,ncore=detectCores()-1,noboot=noboot,nreg=100)
@@ -159,7 +101,7 @@ qob.gs = function(x){
 for (i in 0:1){
   for (j in 1:3){
     t = system.time((q <- qob.gs(data[sex1==i&skill==j])))
-    file.name = paste("002_net_qob_genderskill_",i,j,suffix,".RData",sep="")
+    file.name = paste("002_net_qob_genderskill_",i,j,suffix,out.se,".RData",sep="")
     this.meta.time = rbind(meta.time,list(file.name,as.character(Sys.Date()),t[[1]],t[[2]],t[[3]]))
     fwrite(this.meta.time,meta.time.file,append=T)
     save(q,file = file.name)
@@ -167,19 +109,7 @@ for (i in 0:1){
 }
 rm(q)
 
-# qob01_net = qob.gs(data[sex1==1&low_skill==1])
-# qob02_net = qob.gs(data[sex1==1&med_skill==1])
-# qob03_net = qob.gs(data[sex1==1&high_skill==1])
-# qob04_net = qob.gs(data[sex1==0&low_skill==1])
-# qob05_net = qob.gs(data[sex1==0&med_skill==1])
-# qob06_net = qob.gs(data[sex1==0&high_skill==1])
-# 
-# prov = list(qob01_net,qob02_net,qob03_net,qob04_net,qob05_net,qob06_net)
-# save(prov,file=paste("002_net_qob_genderskill",suffix,".RData",sep=""))
-# rm(prov)
-# rm(list=ls(pattern="qob"))
-
-# 5.3 QOB by race and skill
+# 5.2 QOB by race and skill
 qob.rs = function(x){
   counterfactual(log(hwage1)-effect~sex1+tenure+I(tenure^2)+factor(region)+factor(yr),
                  data=x,group=group1,sepcore=T,treatment=T,decomposition=T,ncore=detectCores()-1,noboot=noboot,nreg=100)
@@ -188,7 +118,7 @@ qob.rs = function(x){
 for (i in 0:1){
   for (j in 1:3){
     t = system.time((q <- qob.rs(data[nonwhite1==i&skill==j])))
-    file.name = paste("003_net_qob_raceskill_",i,j,suffix,".RData",sep="")
+    file.name = paste("003_net_qob_raceskill_",i,j,suffix,out.se,".RData",sep="")
     this.meta.time = rbind(meta.time,list(file.name,as.character(Sys.Date()),t[[1]],t[[2]],t[[3]]))
     fwrite(this.meta.time,meta.time.file,append=T)
     save(q,file = file.name)
@@ -196,14 +126,42 @@ for (i in 0:1){
 }
 rm(q)
 
-# qob07_net = qob.rs(data[nonwhite1==1&low_skill==1])
-# qob08_net = qob.rs(data[nonwhite1==1&med_skill==1])
-# qob09_net = qob.rs(data[nonwhite1==1&high_skill==1])
-# qob10_net = qob.rs(data[nonwhite1==0&low_skill==1])
-# qob11_net = qob.rs(data[nonwhite1==0&med_skill==1])
-# qob12_net = qob.rs(data[nonwhite1==0&high_skill==1])
-# 
-# prov = list(qob07_net,qob08_net,qob09_net,qob10_net,qob11_net,qob12_net)
-# save(prov,file=paste("003_net_qob_raceskill",suffix,".RData",sep=""))
-# rm(prov)
-# rm(list=ls(pattern="qob"))
+#####
+# 6. QOB on whole sample
+#####
+
+## 6.1 QOB on whole sample 
+
+base.qob = function(x){
+  counterfactual(log(hwage1)~nonwhite1+sex1+med_skill+high_skill+tenure+I(tenure^2)+factor(region)+factor(yr),
+                 data=x,group=group1,sepcore=T,treatment=T,decomposition=T,ncore=detectCores()-1,noboot=noboot,nreg=100)
+}
+
+t = system.time(qob00 <- base.qob(data))
+
+file.name=paste("002_qob",suffix,out.se,".RData",sep="")
+this.meta.time = rbind(meta.time,list(file.name,as.character(Sys.Date()),t[[1]],t[[2]],t[[3]]))
+fwrite(this.meta.time,meta.time.file,append=T)
+
+setwd(analysis.dir)
+save(qob00,file=file.name)
+rm(qob00)
+
+## 6.2 QOB net of FEs on whole sample
+base.qob = function(x){
+  counterfactual(log(hwage1)-effect~nonwhite1+sex1+med_skill+high_skill+tenure+I(tenure^2)+factor(region)+factor(yr),
+                 data=x,group=group1,sepcore=T,treatment=T,decomposition=T,ncore=detectCores()-1,noboot=noboot,nreg=100)
+}
+
+t = system.time(qob00_net <- base.qob(data))
+
+file.name=paste("002_net_qob",suffix,out.se,".RData",sep="")
+this.meta.time = rbind(meta.time,list(file.name,as.character(Sys.Date()),t[[1]],t[[2]],t[[3]]))
+fwrite(this.meta.time,meta.time.file,append=T)
+
+setwd(analysis.dir)
+save(qob00_net,file=file.name)
+rm(qob00_net)
+
+
+
